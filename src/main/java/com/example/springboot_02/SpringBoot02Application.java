a9a746a815bcf089f57bcf1a6c31dada28ccb4a7
hujunyi <2692243932@qq.com>
2023-05-06 21:16:17 +0800

    使用Redis cache 缓存菜品套餐

--- src/main/java/com/example/springboot_02/SpringBoot02Application.java
+++ src/main/java/com/example/springboot_02/SpringBoot02Application.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ServletComponentScan
 @EnableTransactionManagement
 @EnableWebMvc
+@EnableCaching
 public class SpringBoot02Application {
 
     public static void main(String[] args) {
